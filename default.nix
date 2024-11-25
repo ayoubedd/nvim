03297f22d54a25512fc4929c1d1c2829ad11925e
Ayoub Eddaoudi <me@ayoubedd.me>
2024-11-25 16:46:25 +0100

    chore: init commit

---
+++ default.nix
@@ -0,0 +1,52 @@
+{ pkgs }: { lib, config, ... }: 
+with lib;
+with pkgs;
+let
+ cfg = config.neovim;
+in
+{
+  options.neovim = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+
+    neovide = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [
+    ] ++ optional cfg.neovide neovide;
+
+    programs.neovim = {
+      package = neovim-unwrapped;
+      enable = true;
+      extraPackages = [
+        # essentials
+        nodejs gcc git cargo tree-sitter ripgrep
+
+        # lsp servers
+        nixd
+        pyright
+        eslint_d
+        pylint
+        rust-analyzer
+        gopls
+        clang-tools
+
+        svelte-language-server
+        typescript-language-server
+        vscode-langservers-extracted
+        emmet-ls
+      ];
+    };
+
+    home.file.".config/nvim" = {
+      source = ./conf;
+      recursive = true;
+    };
+  };
+}
